c519e1a2a6ab4f2fc35f238103ae9f2859c3bb84
Teppei Kuroda <niamugi@gmail.com>
2018-06-22 18:05:46 +0900

    mqttのソースをブローカーとPublish/Subcribeに分けた。 使い勝手に合わせて今後修正していく。

---
+++ BaseProject/Assets/[Fundamenta]/mqtt/mqttBase.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+public class mqttBase : MonoBehaviour {
+    #region Singleton
+
+    private static mqttBase instance;
+
+    public static mqttBase Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (mqttBase)FindObjectOfType(typeof(mqttBase));
+            }
+            return instance;
+        }
+    }
+
+    #endregion Singleton
+
+    [SerializeField]
+    private string brokerIPAddress;
+    [SerializeField]
+    private int brokerPort;
+	private MqttClient client;
+
+    public void Init()
+    {
+        client = null;
+        if (brokerIPAddress.Length <= 0) return;
+        client = new MqttClient(IPAddress.Parse(brokerIPAddress), brokerPort, false, null);
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId);
+    }
+
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        Debug.Log("topic: " + e.Topic);
+        Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+    }
+
+    public void SetSubscribeTopic(string topic)
+    {
+        client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    public void SetPublish(string topic, string msg)
+    {
+        client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(msg), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+}
---
+++ BaseProject/Assets/[Fundamenta]/mqtt/mqttUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mqttUnit : MonoBehaviour
+{
+    [SerializeField]
+    string SubscribeTopic;
+    [SerializeField]
+    string PublishTopic;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Connect"))
+        {
+            Debug.Log("Connect MQTT Server...");
+            mqttBase.Instance.Init();
+            mqttBase.Instance.SetSubscribeTopic(SubscribeTopic);
+            Debug.Log("Success");
+        }
+
+
+        if (GUI.Button(new Rect(20, 70, 80, 20), "Publish"))
+        {
+            Debug.Log("sending...");
+            mqttBase.Instance.SetPublish(PublishTopic, "Sending from Unity3D");
+            Debug.Log("sent");
+        }
+    }
+}
