ca4f0d7a330d163009f6e4e5813e2dfb29492051
Teppei Kuroda <niamugi@gmail.com>
2018-08-06 15:16:12 +0900

    beebotteにもアクセスできるようにした。 subscribeのみ動作確認済み。 設定ファイルを外部に置いた。gitからは除外。StreamingAssetsに.mqtt_settingを置く。

--- BaseProject/Assets/[Fundamenta]/mqtt/mqttBase.cs
+++ BaseProject/Assets/[Fundamenta]/mqtt/mqttBase.cs
@@ -7,6 +7,9 @@ using uPLibrary.Networking.M2Mqtt.Utility;
 using uPLibrary.Networking.M2Mqtt.Exceptions;
 
 using System;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 public class mqttBase : MonoBehaviour {
     #region Singleton
@@ -27,26 +30,70 @@ public class mqttBase : MonoBehaviour {
 
     #endregion Singleton
 
-    [SerializeField]
-    private string brokerIPAddress;
-    [SerializeField]
-    private int brokerPort;
-	private MqttClient client;
+    private string brokerIPAddress = string.Empty;
+    private int brokerPort = 0;
+    private string token = string.Empty;
+    private MqttClient client;
+    private String PublishMsg;
+    [DataContract]
+    public class PublishMsg_Beebotte
+    {
+        [DataMember]
+        public string data { get; set; }
+        [DataMember]
+        public bool ispublic { get; set; }
+        [DataMember]
+        public long ts { get; set; }
+    }
+    public PublishMsg_Beebotte msg = null;
+
+    public DateTime timestamp;
+    public static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    public static DateTime FromUnixTime(long unixTime)
+    {
+        // UNIXエポックからの経過秒数で得られるローカル日付
+        return UNIX_EPOCH.AddSeconds(unixTime).ToLocalTime();
+    }
 
     public void Init()
     {
         client = null;
+
+        getMQTTsetting();
+
         if (brokerIPAddress.Length <= 0) return;
-        client = new MqttClient(IPAddress.Parse(brokerIPAddress), brokerPort, false, null);
+        if (brokerPort <= 0) return;
+        try
+        {
+            //mosquitto
+            client = new MqttClient(IPAddress.Parse(brokerIPAddress), brokerPort, false, null);
+        }
+        catch
+        {
+            //beebotte
+            client = new MqttClient(brokerIPAddress, brokerPort, false, null);
+        }
+
+
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
         string clientId = Guid.NewGuid().ToString();
-        client.Connect(clientId);
+
+        if (token.Length <= 0)
+        {
+            client.Connect(clientId);
+        } else
+        {
+            client.Connect(clientId, token, "");
+        }
     }
 
     void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
     {
         Debug.Log("topic: " + e.Topic);
-        Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+        PublishMsg = System.Text.Encoding.UTF8.GetString(e.Message);
+        Debug.Log("Received: " + PublishMsg);
+        msg = JsonConvert.DeserializeObject<PublishMsg_Beebotte>(PublishMsg);
+        timestamp = FromUnixTime(msg.ts / 1000);
     }
 
     public void SetSubscribeTopic(string topic)
@@ -57,5 +104,29 @@ public class mqttBase : MonoBehaviour {
     public void SetPublish(string topic, string msg)
     {
         client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(msg), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+    
+
+    /// <summary>
+    /// 外部にある設定ファイルを読む
+    /// </summary>
+    void getMQTTsetting()
+    {
+        FileInfo fi = new FileInfo(Application.dataPath + "/StreamingAssets/.mqtt_setting");
+        try
+        {
+            using (StreamReader sr = new StreamReader(fi.OpenRead(), System.Text.Encoding.UTF8))
+            {
+                //1行目：IPアドレス
+                //2行目：ポート
+                //3行目：トークン
+                brokerIPAddress = sr.ReadLine().Split(',')[1];
+                brokerPort = int.Parse(sr.ReadLine().Split(',')[1]);
+                token = "token:" + sr.ReadLine().Split(',')[1];
+            }
+        }
+        catch (Exception e)
+        {
+        }
     }
 }
--- BaseProject/Assets/[Fundamenta]/mqtt/mqttUnit.cs
+++ BaseProject/Assets/[Fundamenta]/mqtt/mqttUnit.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mqttUnit : MonoBehaviour
 {
@@ -8,23 +9,30 @@ public class mqttUnit : MonoBehaviour
     string SubscribeTopic;
     [SerializeField]
     string PublishTopic;
+    [SerializeField]
+    Text txt_ts;
+    [SerializeField]
+    Text txt_data;
 
-    void OnGUI()
+    public void Init()
     {
-        if (GUI.Button(new Rect(20, 40, 80, 20), "Connect"))
-        {
-            Debug.Log("Connect MQTT Server...");
-            mqttBase.Instance.Init();
-            mqttBase.Instance.SetSubscribeTopic(SubscribeTopic);
-            Debug.Log("Success");
-        }
-
-
-        if (GUI.Button(new Rect(20, 70, 80, 20), "Publish"))
-        {
-            Debug.Log("sending...");
-            mqttBase.Instance.SetPublish(PublishTopic, "Sending from Unity3D");
-            Debug.Log("sent");
-        }
+        Debug.Log("Connect MQTT Server...");
+        mqttBase.Instance.Init();
+        mqttBase.Instance.SetSubscribeTopic(SubscribeTopic);
+        Debug.Log("Success");
+        txt_ts.text = "Wait Message...";
+    }
+    public void SetPublish(string _s)
+    {
+        Debug.Log("sending...");
+        mqttBase.Instance.SetPublish(PublishTopic, "Sending from Unity3D");
+        Debug.Log("sent");
+    }
+    private void Update()
+    {
+        if (mqttBase.Instance == null) return;
+        if (mqttBase.Instance.msg == null) return;
+        txt_ts.text = mqttBase.Instance.timestamp.ToString("yyyy-MM-dd HH:mm:ss");
+        txt_data.text = mqttBase.Instance.msg.data;
     }
 }
