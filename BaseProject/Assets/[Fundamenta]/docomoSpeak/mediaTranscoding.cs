2f7922542c55b3e023e60d0d7c4cef6d787383bd
Teppei Kuroda <niamugi@gmail.com>
2019-09-27 16:55:56 +0900

    NAudioはUnityで使えない？落ちる。

--- BaseProject/Assets/[Fundamenta]/docomoSpeak/mediaTranscoding.cs
+++ BaseProject/Assets/[Fundamenta]/docomoSpeak/mediaTranscoding.cs
@@ -1,76 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if ENABLE_WINMD_SUPPORT
-using Windows.Storage;
-using Windows.Media.MediaProperties;
-using Windows.Media.Transcoding;
-#endif
+using NAudio.Wave;
+using System.IO;
 
 public class mediaTranscoding : MonoBehaviour
 {
 
-#if ENABLE_WINMD_SUPPORT
     void hoge()
     {
-        var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
-
-        openPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
-        openPicker.FileTypeFilter.Add(".m4a");
-        openPicker.FileTypeFilter.Add(".wav");
-
-        //StorageFile source = await openPicker.PickSingleFileAsync();
-        StorageFile source = await storageFolder.GetFileAsync("D:\#WorkSpace\#PersonalDevelop\BaseUnityProject\BaseProject\おめでとう.m4a");
-
-        var savePicker = new Windows.Storage.Pickers.FileSavePicker();
-
-        savePicker.SuggestedStartLocation =
-            Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
-
-        savePicker.DefaultFileExtension = ".wav";
-        savePicker.SuggestedFileName = "D:\#WorkSpace\#PersonalDevelop\BaseUnityProject\BaseProject\hoge";
-
-        savePicker.FileTypeChoices.Add("WAVE", new string[] { ".wav" });
-
-        StorageFile destination = await savePicker.PickSaveFileAsync();
-
-        MediaEncodingProfile profile =
-                MediaEncodingProfile.CreateWav(AudioEncodingQuality.Medium);
-        MediaTranscoder transcoder = new MediaTranscoder();
-        PrepareTranscodeResult prepareOp = await
-            transcoder.PrepareFileTranscodeAsync(source, destination, profile);
-        if (prepareOp.CanTranscode)
-        {
-            var transcodeOp = prepareOp.TranscodeAsync();
-            transcodeOp.Progress +=
-                new AsyncActionProgressHandler<double>(TranscodeProgress);
-            transcodeOp.Completed +=
-                new AsyncActionWithProgressCompletedHandler<double>(TranscodeComplete);
-        }
-        else
-        {
-            switch (prepareOp.FailureReason)
-            {
-                case TranscodeFailureReason.CodecNotFound:
-                    System.Diagnostics.Debug.WriteLine("Codec not found.");
-                    break;
-                case TranscodeFailureReason.InvalidProfile:
-                    System.Diagnostics.Debug.WriteLine("Invalid profile.");
-                    break;
-                default:
-                    System.Diagnostics.Debug.WriteLine("Unknown failure.");
-                    break;
-            }
-        }        
+        var path = "おめでとう_noHeader.m4a";
+        var s = new RawSourceWaveStream(File.OpenRead(path), new WaveFormat(16000, 1));
+        var outpath = "example_noHeader_2.wav";
+        WaveFileWriter.CreateWaveFile(outpath, s);
     }
-#endif
+
+    void hogehoge()
+    {
+        WaveFormat format = new WaveFormat(16000, 16, 1);
+        try
+        {
+            using (MediaFoundationReader reader = new MediaFoundationReader("D:\\#WorkSpace\\#PersonalDevelop\\BaseUnityProject\\BaseProject\\ykbr0-i5fv9.wav"))
+            {
+                using (MediaFoundationResampler resampler = new MediaFoundationResampler(reader, format))
+                {
+                    WaveFileWriter.CreateWaveFile("sample.wav", resampler);
+                }
+            }
+
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    void hogehogehoge()
+    {
+        //https://codeday.me/jp/qa/20190626/1109062.html
+
+        // convert source audio to AAC
+        // create media foundation reader to read the source (can be any supported format, mp3, wav, ...)
+        using (MediaFoundationReader reader = new MediaFoundationReader(@"d:\ykbr0-i5fv9.wav"))
+        {
+            MediaFoundationEncoder.EncodeToAac(reader, @"D:\test.mp4");
+        }
+
+        // convert "back" to WAV
+        // create media foundation reader to read the AAC encoded file
+        using (MediaFoundationReader reader = new MediaFoundationReader(@"D:\test.mp4"))
+        // resample the file to PCM with same sample rate, channels and bits per sample
+        using (ResamplerDmoStream resampledReader = new ResamplerDmoStream(reader,
+            new WaveFormat(reader.WaveFormat.SampleRate, reader.WaveFormat.BitsPerSample, reader.WaveFormat.Channels)))
+        // create WAVe file
+        using (WaveFileWriter waveWriter = new WaveFileWriter(@"d:\test.wav", resampledReader.WaveFormat))
+        {
+            // copy samples
+            resampledReader.CopyTo(waveWriter);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-#if ENABLE_WINMD_SUPPORT
-        Debug.Log("hoge");
-        hoge();
-#endif
+        hogehogehoge();
     }
 
     // Update is called once per frame
