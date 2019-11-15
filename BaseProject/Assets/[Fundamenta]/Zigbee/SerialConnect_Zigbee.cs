418c4c6429a2b65f27737fd621e423db593550a5
Teppei Kuroda <niamugi@gmail.com>
2019-11-15 15:29:00 +0900

    Dice Demo 追加

--- BaseProject/Assets/[Fundamenta]/Zigbee/SerialConnect_Zigbee.cs
+++ BaseProject/Assets/[Fundamenta]/Zigbee/SerialConnect_Zigbee.cs
@@ -3,24 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-public class SerialConnect_Zigbee : MonoBehaviour
-{
+public class SerialConnect_Zigbee : MonoBehaviour {
 
     #region Singleton
 
     private static SerialConnect_Zigbee instance;
 
-    public static SerialConnect_Zigbee Instance_Zigbee
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = (SerialConnect_Zigbee)FindObjectOfType(typeof(SerialConnect_Zigbee));
+    public static SerialConnect_Zigbee Instance_Zigbee {
+        get {
+            if (instance == null) {
+                instance = (SerialConnect_Zigbee) FindObjectOfType (typeof (SerialConnect_Zigbee));
 
-                if (instance == null)
-                {
-                    Debug.LogError(typeof(SerialConnect_Zigbee) + "is nothing");
+                if (instance == null) {
+                    Debug.LogError (typeof (SerialConnect_Zigbee) + "is nothing");
                 }
             }
             return instance;
@@ -28,7 +23,6 @@ public class SerialConnect_Zigbee : MonoBehaviour
     }
 
     #endregion Singleton
-
 
     /// <summary>
     /// SerialHandler.serial_unit
@@ -49,105 +43,98 @@ public class SerialConnect_Zigbee : MonoBehaviour
     /// シリアル通信が出来ているかチェック
     /// </summary>
     /// <returns></returns>
-    public bool GetisConnect()
-    {
+    public bool GetisConnect () {
         return isConnect;
     }
 
-    public string DebugList()
-    {
+    public string DebugList () {
         if (_serial == null) return string.Empty;
 
-        System.Text.StringBuilder sb = new System.Text.StringBuilder();
-        sb.Append("--- CONNECT ZIGBEE INFO ---");
-        sb.Append("\n");
+        System.Text.StringBuilder sb = new System.Text.StringBuilder ();
+        sb.Append ("--- CONNECT ZIGBEE INFO ---");
+        sb.Append ("\n");
 
-        sb.Append("[DICE]");
-        sb.Append("\n (ID, Number) : (");
-        sb.Append(dice[0]);
-        sb.Append(", ");
-        sb.Append(dice[1]);
-        sb.Append(")");
-        sb.Append("\n");
+        if (isConnect) {
+            sb.Append ("[DICE]");
+            sb.Append ("\n (ID, Number) : (");
+            sb.Append (dice[0]);
+            sb.Append (", ");
+            sb.Append (dice[1]);
+            sb.Append (")");
+            sb.Append ("\n");
 
-        sb.Append("Battery");
-        sb.Append("\n");
-        sb.Append(battery);
-        sb.Append("\n");
+            sb.Append ("Battery");
+            sb.Append ("\n");
+            sb.Append (battery);
+            sb.Append ("\n");
 
-        sb.Append("[GetData]");
-        sb.Append("\n");
-        sb.Append(GetData);
-        sb.Append("\n");
+            sb.Append ("[GetData]");
+            sb.Append ("\n");
+            sb.Append (GetData);
+            sb.Append ("\n");
+        } else {
+            sb.Append ("DisConnect");
+            sb.Append ("\n");
+        }
 
-        return sb.ToString();
+        return sb.ToString ();
     }
 
-    void DebugMsg(string head, string _s)
-    {
+    void DebugMsg (string head, string _s) {
         string msg = head + _s;
 
-        byte[] _tmp = System.Text.Encoding.ASCII.GetBytes(_s);
+        byte[] _tmp = System.Text.Encoding.ASCII.GetBytes (_s);
         msg += " Length:" + _tmp.Length;
 
         msg += " Hex:";
-        foreach (byte b in _tmp)
-        {
-            msg += Convert.ToString(b, 16) + " - ";
+        foreach (byte b in _tmp) {
+            msg += Convert.ToString (b, 16) + " - ";
         }
 
-        Debug.LogWarning(msg);
+        Debug.LogWarning (msg);
     }
 
-
-    public void StartInit()
-    {
+    public void StartInit () {
         _serial = null;
         isConnect = false;
     }
 
-    public void Connect()
-    {
-        if (NowCoroutine != null) StopCoroutine(NowCoroutine);
-        NowCoroutine = StartCoroutine(ConnectCoroutine());
+    public void Connect () {
+        if (NowCoroutine != null) StopCoroutine (NowCoroutine);
+        NowCoroutine = StartCoroutine (ConnectCoroutine ());
     }
 
-    private IEnumerator ConnectCoroutine()
-    {
-        var wait = new WaitForSeconds(0.1f);
+    private IEnumerator ConnectCoroutine () {
+        var wait = new WaitForSeconds (0.1f);
         isConnect = false;
         yield return wait;
 
-        SerialPortName _sp = GetComponent<SerialPortName>();
+        SerialPortName _sp = GetComponent<SerialPortName> ();
         if (_sp == null) yield break;
-        _serial = SerialHandler.Instance.PortList[_sp.SerialListNo];   //SerialHandlerのリストと紐づく
+        _serial = SerialHandler.Instance.PortList[_sp.SerialListNo]; //SerialHandlerのリストと紐づく
 
         //USBの切断
-        _serial.Close();
+        _serial.Close ();
         _serial.OnDataReceived -= OnDataReceived;
         yield return wait;
 
-
         //USBの接続
-        _serial.Open(true);
-        _serial.OnDataReceived += OnDataReceived;
-
-        isConnect = true;
+        if (_serial.Open (true)) {
+            _serial.OnDataReceived += OnDataReceived;
+            isConnect = true;
+        };
         yield break;
 
     }
 
-    public void DataSend(string _s)
-    {
+    public void DataSend (string _s) {
         if (_serial == null) return;
         //DebugMsg("[DataSend] SendData ", _send);
-        _serial.Write(_s);
+        _serial.Write (_s);
     }
 
-
     //受信した信号(message)に対する処理
-    void OnDataReceived(string[] message)
-    {
+    void OnDataReceived (string[] message) {
         //今回受信するデータの形式
         //::rc=80000000:lq=189:ct=006D:ed=810D33EA:id=3:ba=2490:a1=1237:a2=0576:x=0001:y=0000:z=0000
         //欲しい情報はIDとxの値だけなので、そのペアを見つける。
@@ -155,30 +142,35 @@ public class SerialConnect_Zigbee : MonoBehaviour
 
         //メッセージを保存
         GetData = string.Empty;
-        foreach (string _t in message)
-        {
+        foreach (string _t in message) {
             GetData += _t;
         }
 
         //update処理内で解析中の場合は以下の処理を行わない
         if (isAnalysis) return;
 
-        int i = GetData.LastIndexOf(":id="); //一番後ろ(最新)のIDを取得する
-        int j = GetData.LastIndexOf(":x="); //一番後ろ(最新)のxを取得する
-        if (i > 0 && i < j)
-        {
-            dice[0] = (char)GetData[i + 4] - 48; //0のasciiコードが48
-            dice[1] = (char)GetData[j + 6] - 48;
+        int i = GetData.LastIndexOf (":id="); //一番後ろ(最新)のIDを取得する
+        int j = GetData.LastIndexOf (":x="); //一番後ろ(最新)のxを取得する
+        if (i > 0 && i < j) {
+            dice[0] = (char) GetData[i + 4] - 48; //0のasciiコードが48
+            dice[1] = (char) GetData[j + 6] - 48;
         }
 
-        i = GetData.LastIndexOf(":ba=");
-        j = GetData.LastIndexOf(":a1=");
-        if (i > 0 && i < j)
-        {
-            string ba = GetData.Substring(i + 4, j - (i + 4));
-            battery = Int16.Parse(ba);
+        i = GetData.LastIndexOf (":ba=");
+        j = GetData.LastIndexOf (":a1=");
+        if (i > 0 && i < j) {
+            string ba = GetData.Substring (i + 4, j - (i + 4));
+            battery = Int16.Parse (ba);
         }
     }
 
+    public void resetDiceValue () {
+        dice[0] = -1;
+        dice[1] = -1;
+    }
+
+    public int getDiceValue () {
+        return dice[1];
+    }
 
 }
---
+++ BaseProject/Assets/[Fundamenta]/Zigbee/diceDemo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class diceDemo : MonoBehaviour {
+    [SerializeField]
+    Text txtVal;
+
+    [SerializeField]
+    Text txtInfo;
+
+    bool isPlay;
+    int loopVal;
+    int lastVal;
+    float waitTime;
+
+    public void btn_start () {
+        if (SerialConnect_Zigbee.Instance_Zigbee == null) return;
+
+        isPlay = true;
+        waitTime = 0.0f;
+        if (SerialConnect_Zigbee.Instance_Zigbee.GetisConnect ()) {
+            SerialConnect_Zigbee.Instance_Zigbee.resetDiceValue ();
+            lastVal = SerialConnect_Zigbee.Instance_Zigbee.getDiceValue ();
+        }
+    }
+
+    public void btn_reconnect () {
+        if (SerialConnect_Zigbee.Instance_Zigbee == null) return;
+        SerialConnect_Zigbee.Instance_Zigbee.Connect ();
+    }
+    public void btn_Exit () {
+        Application.Quit ();
+    }
+
+    string getInfo () {
+        StringBuilder sb = new StringBuilder ();
+        sb.Append (SerialConnect_Zigbee.Instance_Zigbee.DebugList ());
+        return sb.ToString ();
+    }
+
+    void updateValue () {
+        int v = 0;
+        if (isPlay) {
+            loopVal = (++loopVal) % 6 + 1;
+            v = loopVal;
+
+            // 一定時間はダイスの目を受け付けない。
+            waitTime += Time.deltaTime;
+            if (waitTime > 2.0f) {
+                if (SerialConnect_Zigbee.Instance_Zigbee.getDiceValue () != lastVal) {
+                    isPlay = false;
+                }
+            }
+        } else {
+            v = SerialConnect_Zigbee.Instance_Zigbee.getDiceValue ();
+        }
+        txtVal.text = v.ToString ();
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        isPlay = false;
+        loopVal = 1;
+        lastVal = 1;
+        btn_reconnect ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (SerialConnect_Zigbee.Instance_Zigbee == null) return;
+
+        // Info
+        txtInfo.text = getInfo ();
+
+        if (SerialConnect_Zigbee.Instance_Zigbee.GetisConnect ()) {
+            updateValue ();
+        }
+
+    }
+}
