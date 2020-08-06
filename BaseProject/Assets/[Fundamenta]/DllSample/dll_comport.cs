9e99acf94403c9f56ad33198af98406726a2c027
Teppei Kuroda <niamugi@gmail.com>
2020-08-06 10:41:02 +0900

    デバイス名が1文字しか取得できない

--- BaseProject/Assets/[Fundamenta]/DllSample/dll_comport.cs
+++ BaseProject/Assets/[Fundamenta]/DllSample/dll_comport.cs
@@ -1,19 +1,21 @@
 ﻿// 参考URL [UnityでC++を使う方法]
 // https://qiita.com/8128/items/7cd0bf0b3f5bad60f709
-using UnityEngine;
 using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
 
-static class Lib
-{
-    [DllImport("DllComport", CallingConvention = CallingConvention.StdCall)]
-    public static extern string getComportList();
+static class Lib {
+    [DllImport ("DllComport", CallingConvention = CallingConvention.StdCall)]
+    public static extern void getComportList (StringBuilder ret, int retLen);
 }
 
-public class dll_comport : MonoBehaviour
-{
+public class dll_comport : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(Lib.getComportList());
+    void Start () {
+        int STRING_MAX_LENGTH = 512;
+        StringBuilder str = new StringBuilder (STRING_MAX_LENGTH);
+
+        Lib.getComportList (str, STRING_MAX_LENGTH);
+        Debug.Log (str.ToString ());
     }
 }
