f2d9019deb5f7f2edee7a040d6073e2f02417086
Teppei Kuroda <niamugi@gmail.com>
2020-08-06 17:55:27 +0900

    ポート名を取得できる時と出来ない時があった

--- BaseProject/Assets/[Fundamenta]/DllSample/dll_comport.cs
+++ BaseProject/Assets/[Fundamenta]/DllSample/dll_comport.cs
@@ -4,18 +4,29 @@ using System.Runtime.InteropServices;
 using System.Text;
 using UnityEngine;
 
-static class Lib {
-    [DllImport ("DllComport", CallingConvention = CallingConvention.StdCall)]
-    public static extern void getComportList (StringBuilder ret, int retLen);
+static class Lib
+{
+    [DllImport("DllComport", CallingConvention = CallingConvention.StdCall)]
+    public static extern void getComportList(StringBuilder ret, int retLen);
 }
 
-public class dll_comport : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start () {
-        int STRING_MAX_LENGTH = 512;
-        StringBuilder str = new StringBuilder (STRING_MAX_LENGTH);
-
-        Lib.getComportList (str, STRING_MAX_LENGTH);
-        Debug.Log (str.ToString ());
+public class dll_comport : MonoBehaviour
+{
+    bool isGet;
+    int STRING_MAX_LENGTH = 512;
+    void Start()
+    {
+        isGet = false;
     }
+
+    void Update()
+    {
+        if (isGet) return;
+        StringBuilder str = new StringBuilder(STRING_MAX_LENGTH);
+        Lib.getComportList(str, STRING_MAX_LENGTH);
+        Debug.Log(str.Length);
+        Debug.Log(str.ToString());
+        if (str.Length > 0) isGet = true;
+    }
+
 }
